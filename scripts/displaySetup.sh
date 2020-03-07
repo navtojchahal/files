9e543671c165a2d537938686a602d4c69b8ee42f
Navtoj Chahal <37762455+navtojchahal@users.noreply.github.com>
2020-03-07 15:31:45 -0800

    Update displaySetup.sh

--- scripts/displaySetup.sh
+++ scripts/displaySetup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 internal=eDP-1
-external=HDMI-1
-extra=DP-1
+external=DP-1
+extra=DP-2
 
 if xrandr | grep "$external connected"; then
 echo
     if [ "$#" -eq "0" ]; then
         echo "External Monitor: ACER Monitor"
-        xrandr --output $internal --mode 1366x768 --pos 157x1050 --rotate normal --output $external --primary --mode 1680x1050 --pos 0x0 --rotate normal --output $extra --off
+        xrandr --output $internal --mode 1920x1200 --pos -120x1050 --rotate normal --output $external --primary --mode 1680x1050 --pos 0x0 --rotate normal --output $extra --off
         echo
     elif [ "$#" -eq "1" ]; then
         if [ "$1" -le "5" ]; then
             case $1 in
                 0)
                     echo "External Monitor: None"
-                    xrandr --output $internal --primary --mode 1366x768 --pos 0x0 --rotate normal --output $external --off --output $extra --off
+                    xrandr --output $internal --primary --mode 1920x1200 --pos 0x0 --rotate normal --output $external --off --output $extra --off
                     echo;;
 				1|a|A)
                     echo "External Monitor: Haier TV"
-                    xrandr --output $internal --mode 1366x768 --pos 277x1080 --rotate normal --output $external --primary --mode 1920x1080 --pos 0x0 --rotate normal --output $extra --off
+                    xrandr --output $internal --mode 1920x1200 --pos 0x1080 --rotate normal --output $external --primary --mode 1920x1080 --pos 0x0 --rotate normal --output $extra --off
                     echo;;
                 *)
                     echo "The options are:    1|a|A) Haier TV"
@@ -34,7 +34,7 @@ echo
         echo
     fi
 else
-	xrandr --output $internal --primary --mode 1366x768 --pos 0x0 --rotate normal --output $external --off --output $extra --off
+	xrandr --output $internal --primary --mode 1920x1200 --pos 0x0 --rotate normal --output $external --off --output $extra --off
 fi
 
 # Number of total arguments # echo "Total number of arguments: $#"
